4281f99b4f8cca4b0870f8c9ed7e8a2ede8c5f94
Ruben Bielen <ruben.bielen@gmail.com>
2018-10-02 22:57:31 +0200

    Config and setup

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,20 +3,18 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'rubenbielen-console',
+    title: "rubenbielen-console",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Webapp console' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "Webapp console" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
   /*
   ** Customize the progress bar color
   */
-  loading: { color: '#3B8070' },
+  loading: { color: "#3B8070" },
   /*
   ** Build configuration
   */
@@ -24,16 +22,15 @@ module.exports = {
     /*
     ** Run ESLint on save
     */
-    extend (config, { isDev, isClient }) {
-      if (isDev && isClient) {
+    extend(config, { isDev }) {
+      if (isDev && process.client) {
         config.module.rules.push({
-          enforce: 'pre',
+          enforce: "pre",
           test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
+          loader: "eslint-loader",
           exclude: /(node_modules)/
         })
       }
     }
   }
 }
-
